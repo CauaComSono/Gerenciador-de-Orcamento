91456e6b2d4104565ef83ec4005be9687c0e93ff
Cauã Eduardo K. Schadeck <130913401+CauaComSono@users.noreply.github.com>
2024-09-22 22:30:56 -0300

    final deploy

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+
+const Sobre = () => {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <img src="https://www.rsagora.com.br/wp-content/uploads/2023/09/f4f87e1e-cca8-4090-8a9c-2df80bdf65d2.jpg" alt="" />
+            <h2>Nome: Cauã Eduardo Kuhn Schadeck</h2>
+            <h3>Profissão: Auxiliar de TI</h3>              
+            <a href="https://github.com/CauaComSono">GitHub</a>
+        </div>
+    )
+}
+
+export default Sobre;
